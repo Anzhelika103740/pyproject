c757471fced25262534b54f377feb3dc0d2df502
anzhelika2022 <anzhelikatosheva@ua.pt>
2022-12-02 01:48:23 +0000

    ex 1+ 2

--- ex1.py
+++ ex1.py
@@ -1,2 +1,14 @@
-print("Exercise12")
-#saaa
+
+def mors(num1, num2):
+    product = num1 * num2
+    if product <= 1000:
+        return product
+    else:
+        #if product is too high, calculate the sum
+        return num1 + num2
+
+result = mors(30,10)
+print("The result is ", result)
+
+result = mors(30,50)
+print("The result is ", result)
